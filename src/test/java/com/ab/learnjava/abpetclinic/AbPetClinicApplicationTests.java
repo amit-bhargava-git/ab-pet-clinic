a083e3faec72907ca5c6eada0544164e5fb69b80
Amit Bhargava <amit.bhargava@absg.com>
2020-07-07 20:04:14 -0500

    Initial project checkin

---
+++ src/test/java/com/ab/learnjava/abpetclinic/AbPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ab.learnjava.abpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
